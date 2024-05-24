07c27ea84c9596ee03f0d3820ede7918869d0ba4
FloBer57 <115531870+FloBer57@users.noreply.github.com>
2024-05-24 16:51:18 +0200

    Ajout gestion notification SignalR

--- QuackersAPI_DDD/API/Hub/SignalrHub.cs
+++ QuackersAPI_DDD/API/Hub/SignalrHub.cs
@@ -1,6 +1,7 @@
 ﻿namespace QuackersAPI_DDD.API.Hub
 {
     using Microsoft.AspNetCore.SignalR;
+    using QuackersAPI_DDD.Domain.Model;
 
     public class SignalrHub : Hub
     {
@@ -18,5 +19,20 @@
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, channelId.ToString());
         }
+
+        public async Task SendNotification(int notificationId, string notificationName, string notificationText, int notificationType)
+        {
+
+            await Clients.AllExcept(notificationId.ToString()).SendAsync("ReceiveNotification", new
+            {
+                NotificationId = notificationId,
+                NotificationName = notificationName,
+                NotificationText = notificationText,
+                NotificationType = notificationType
+            });
+
+        }
     }
+
 }
+
