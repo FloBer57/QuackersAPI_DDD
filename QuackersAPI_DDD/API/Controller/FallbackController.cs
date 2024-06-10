5d71def0c97d2d6ac7593c666ccfd2854903850d
Tardice <florent.bernar@outlook.fr>
2024-06-10 22:29:38 +0200

    add

---
+++ QuackersAPI_DDD/API/Controller/FallbackController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuackersAPI_DDD.API.Controller
+{
+    public class FallbackController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+        }
+    }
+}
